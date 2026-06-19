9b8d84bb96da75e7ba35f80daa70398a10cdadcd
suneel <suneelbayya@gmail.com>
2026-06-19 23:06:40 +0530

    Initial commit

---
+++ supplierservice/src/main/java/ecommerce/springboot/microservice/supplierservice/SupplierserviceApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.springboot.microservice.supplierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierserviceApplication.class, args);
+	}
+
+}
---
+++ supplierservice/src/test/java/ecommerce/springboot/microservice/supplierservice/SupplierserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.springboot.microservice.supplierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
